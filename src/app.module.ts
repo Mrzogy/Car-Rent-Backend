7d8a2b872da74db8e87698b5b0566a5e4aedbb4a
Abdulrazaq I. Ali <a.ali.t@scfhs.org.sa>
2025-05-14 12:14:32 +0300

    Mke repo entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,14 @@ import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './users/user.entity';
+import { ReportEntity } from './reports/report.entity';
 
 
 @Module({
   imports: [TypeOrmModule.forRoot({
     type:'sqlite',
     database:'db.sqlite',
-    entities:[UserEntity],
+    entities:[UserEntity,ReportEntity],
     synchronize:true,
   }),
     UsersModule,
